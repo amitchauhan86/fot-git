f04726c24c9c7db5e98144339145523d5659658b
Amit chauhan <amit@cwebconsultants.com>
2025-03-17 17:29:33 +0530

    change

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Byddddddddddd{" "}
+            Byddddddddddd main chnage kiya {" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
